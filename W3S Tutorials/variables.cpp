538f55446e76e1f9aa5b8eecc152920a9a168291
Nico <69917886+Sir-Nico@users.noreply.github.com>
2023-09-14 18:39:44 +0200

    Deleted launch.json >:), also finished variables.

--- W3S Tutorials/variables.cpp
+++ W3S Tutorials/variables.cpp
@@ -10,7 +10,7 @@ int main() {
     myOtherNum = 420; // Assigning the variable a value
     cout << myOtherNum << endl;
 
-    int aNum = 5;               // Integer (whole number without decimals)
+    int aNum = 5;                // Integer (whole number without decimals)
     double myFloatNum = 5.99;    // Floating point number (with decimals)
     char myLetter = 'D';         // Character
     string myText = "Hello";     // String (remember, a string is just a string of chars)
@@ -25,4 +25,16 @@ int main() {
     int y = 10;
     int sum = x + y + 2;
     cout << "The sum of " << x << " + " << y << " is " << sum;  // You Stoopid
+
+    // Holy shit multiple variable declarations at the SAME TIME!!
+    int x = 5, y = 6, z = 50;
+    cout << x + y + z;
+
+    // Same thing as above, just a different way to do it
+    int x, y, z;
+    x = y = z = 50;
+    cout << x + y + z;
+
+    // Constant variable, ergo immutable, read-only, the whole shabang
+    const int PI = 3.141592; //...
 }
