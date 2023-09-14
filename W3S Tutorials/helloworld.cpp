4a6cd2cc4fba7b9765d00f08c958809be8b2aec6
Sir-Nico <rennemo@proton.me>
2023-09-14 01:47:29 -0700

    Commented the shit out of helloworld.cpp and added a variable tutorial program

--- W3S Tutorials/helloworld.cpp
+++ W3S Tutorials/helloworld.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <iostream>  // iostream is used for cout, cin among other things
 
-int main() {
-    std::cout << "𒀲" << std::endl;
-    return 0;
+// using namespace std; // I've commented it out this time, but using this line you can save yourself from typing std:: before everything in the standard library
+
+/* Multiline comment
+WOOHOO!!!!!!!!!!! */
+
+int main() {  // The main function is what is actually ran
+    std::cout << "𒀲" << std::endl;  // std::cout is used to print text to the console, and std::endl is a way to force a newline
+    std::cout << "𒀲\n";  // Prints the same as the line above, just using \n for newline
+    return 0;  // Return code 0 is universally considered the "everything well" exit code, anything else and you are screwed
 }
---
+++ W3S Tutorials/variables.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int myNum = 69; // Defining the variable and assigning a value immediately 
+    cout << myNum << endl;
+
+    int myOtherNum; // Defining the variable without assigning a value
+    myOtherNum = 420; // Assigning the variable a value
+    cout << myOtherNum << endl;
+
+    int aNum = 5;               // Integer (whole number without decimals)
+    double myFloatNum = 5.99;    // Floating point number (with decimals)
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String (remember, a string is just a string of chars)
+    bool myBoolean = true;       // Boolean (true or false) 
+
+    // Using a variable together with strings
+    int age = 17;
+    cout << "I am " << age << " years old.\n";
+
+    // Maffemafics
+    int x = 9;
+    int y = 10;
+    int sum = x + y + 2;
+    cout << "The sum of " << x << " + " << y << " is " << sum;  // You Stoopid
+}
