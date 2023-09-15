25f322f24fd4db9506068ecf4883de461a7efb60
Sir-Nico <rennemo@proton.me>
2023-09-15 14:54:58 +0200

    done with datatypes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cpp_proj
 siuuuu
+
+W3S Tutorials folder is the place where i store all lessons about c++
+Because I am still learning this godforsaken language
--- W3S Tutorials/datatypes.cpp
+++ W3S Tutorials/datatypes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>  // You need to include the string library for strings to properly work
 
 using namespace std;
 
@@ -6,13 +7,16 @@ int main() {
     // basic data types
     int aNum = 5;              // Integer (whole number without decimals)
     float myFloatNum = 9.95;   // Floating point number (with decimals) but 4 bytes of memory (under 8 decimals)
-    double myFloatNum = 5.99;  // Floating point number (with decimals) but 8 bytes of memory (>8 decimals)
-    char myLetter = 'D';       // Character
-    string myText = "Hello";   // String (remember, a string is just a string of chars)
+    double myDoubleNum = 5.99;  // Floating point number (with decimals) but 8 bytes of memory (>8 decimals)
+    char myLetter = 'D';       // Character (single quotes only!)
+    string myText = "Hello";   // String (remember, a string is just a string of chars) (Double quotes only!)
     bool myBoolean = true;     // Boolean (true or false)
 
     // It is possible to define a float with scientific notation
     float f1 = 35e3;
     double d1 = 12E4;
 
+    // chars can also be defined by ASCII character codes
+    char a = 65;  // ASCII character code for 'A'
+    cout << a;
 }
