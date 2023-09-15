b44aa2646d2b4cef0ce972e82db31ccdaaeec6e4
Nico <69917886+Sir-Nico@users.noreply.github.com>
2023-09-15 23:32:56 +0200

    Some math stuff

---
+++ W3S Tutorials/math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>  // Library with a bunch of math functions
+
+using namespace std;
+
+int main() {
+    // Math functions waheyy
+    cout << max(5, 10) << endl;  // Returns the highest value (10)
+    cout << min(5, 10) << endl;  // Does the opposite of max (returns 5)
+
+    // Cmath stuff
+    cout << sqrt(64) << endl;   // Returns square root
+    cout << round(2.5) << endl; // Rounds whatever is input
+    cout << log(M_E) << endl;   // Returns natural log
+
+    return 0;
+}
