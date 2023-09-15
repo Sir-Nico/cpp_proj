cc76a8cd4137c924da5ef1c652dfb3b4343b712b
Sir-Nico <rennemo@proton.me>
2023-09-15 15:14:34 +0200

    Operators done woohoo!

---
+++ W3S Tutorials/operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Mainly just for looking at bitwise shift
+    int x = 5;
+    x <<= 4;  // Shifts the number 4 bits to the left, taking it from 101 (5) to 1010000 (80)
+    cout << x << endl;
+
+    int y = 80;
+    y >>= 4;  // Shifts the number 4 bits to the right, taking it from 1010000 (80) to 101 (5)
+    cout << y;
+
+    // Comparing operators are the same as python:
+    // == : Equal (4 == 6)
+    // != : Not equal (4 != 6)
+    //  < : Less than (4 < 6)
+    // <= : Less or equal to (4 <= 6)
+    //  > : Greater than (4 > 6)
+    // >= : Greater or equal to (4 >= 6)
+    
+    // Logical operators:
+    // && : And (x && y)
+    // || : Or (x || y)
+    //  ! : Not (!(4 == 6), this would return true) 
+
+}
