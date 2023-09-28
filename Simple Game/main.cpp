096ec7d41ac2b1a76c4323d8d8241aa8f116785e
Sir-Nico <rennemo@proton.me>
2023-09-28 21:32:40 +1400

    Added some comments

--- Simple Game/main.cpp
+++ Simple Game/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Returns a number between 1 and 9 (tile)
 int chooseTile() {
 
     int tileNum;
@@ -11,6 +12,8 @@ int chooseTile() {
 
     cout << "Choose a column. (x-axis) \n>> ";
     cin >> tileNum;
+
+    // The line "cout << "\033[1A" << "\033[2K" << "\033[1A" << "\033[2K" << "\r";" is used to clear 2 lines, to erase text from previous prompt
 
     // Forcing user to input numbers between 1 and 3
     isValid = (1 <= tileNum && tileNum <= 3);
@@ -38,12 +41,14 @@ int chooseTile() {
     return tileNum;
 };
 
-void showGrid(char grid[3][3], char currentTurn, bool clearScreen) {  // Function for displaying the grid and its contents
+// Outputs the grid to the console with some extra information (which numbers correspond to tiles)
+void showGrid(char grid[3][3], char currentTurn, bool clearScreen) {
     
     if (clearScreen) {
         cout << "\033[2J\033[H";  // Clears the entire terminal screen, and sets cursor position to top left corner
     };
     
+    // The recurring \033 statements are ANSI escape sequences, where [4m is used to make text underline, [3m is used to make text cursive and [0m resets text effects
     cout << "\033[1mTic Tac Terminal++\033[0m \n";
     cout << "\033[4m" << grid[2][0] << "|" << grid[2][1] << "|" << grid[2][2] << "\033[0m\033[3m 3 \033[0m" << "\n";
     cout << "\033[4m" << grid[1][0] << "|" << grid[1][1] << "|" << grid[1][2] << "\033[0m\033[3m 2 \033[0m" << "\n";
@@ -51,6 +56,7 @@ void showGrid(char grid[3][3], char currentTurn, bool clearScreen) {  // Functio
     cout << "1 2 3 \033[0m \n";
 };
 
+// Changes turn
 char changeTurn(char turn) {
     
     if (turn == 'X') {
@@ -60,6 +66,7 @@ char changeTurn(char turn) {
     return 'X';
 };
 
+// Returns whether the given array consists of the same elements (except ' ')
 bool isEqual(char array[3]) {  // Could make the array variable in size, but getting the size is a bit forked
 
     char controlChar = array[0];  // Uses the first item in the array as the control
@@ -75,7 +82,9 @@ bool isEqual(char array[3]) {  // Could make the array variable in size, but get
     return true;
 };
 
+// Checks if a winning combination has been played by either party
 bool checkWinner(char grid[3][3]) {
+
     // Horizontal
     for (int i = 0; i < 3; i++) {
         char tiles[3];
@@ -121,10 +130,10 @@ bool checkWinner(char grid[3][3]) {
 
 int main() {
 
-    char grid[3][3] = {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}
+    char grid[3][3] = {   // Corresponding tile numbers:
+        {' ', ' ', ' '},  //  7  8  9
+        {' ', ' ', ' '},  //  4  5  6
+        {' ', ' ', ' '}   //  1  2  3
     };  // 2-dimensional array for the grid
     
     int availableTiles = 9;
@@ -133,13 +142,14 @@ int main() {
 
     showGrid(grid, currentTurn, false);  // Need to avoid clearing console immediately, as program doesn't run in terminal window otherwise
     
+    // Game loop, only runs 9 times as there only are 9 tiles on the board
     while (availableTiles > 0) {
         int tile = chooseTile();
         // "unpacking" the tile number to grid indeces (don't know why the column one needs -4, but we move)
         int row = (tile-1) / 3;
         int column = tile - (row-1)*3 - 4;
 
-        int selectedIsEmpty = (grid[row][column] == ' ');
+        int selectedIsEmpty = (grid[row][column] == ' ');  // Checking if the selected tile is empty, and requiring a new tile selected in such a case
         while (!selectedIsEmpty) {
             cout << "Selected tile already occupied. \n>> ";
             getch();
@@ -160,6 +170,7 @@ int main() {
         currentTurn = changeTurn(currentTurn);
         availableTiles--;
     }
+    
     if (!isWinner) {
         cout << "It was a draw!";
         getch();
